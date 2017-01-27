94927303f3b6f3574a9d88890613a896f59b3c1c
Mathew White <mathew.white@mathew-white.local>
2017-01-27 12:20:28 +0000

    Flow done minus robot pic

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,3 +1,90 @@
+(function($){
+
+  /* addClass shim
+   ****************************************************/
+  var addClass = $.fn.addClass;
+  $.fn.addClass = function(value) {
+    var orig = addClass.apply(this, arguments);
+
+    var elem,
+      i = 0,
+      len = this.length;
+
+    for (; i < len; i++ ) {
+      elem = this[ i ];
+      if ( elem instanceof SVGElement ) {
+        var classes = $(elem).attr('class');
+        if ( classes ) {
+            var index = classes.indexOf(value);
+            if (index === -1) {
+              classes = classes + " " + value;
+              $(elem).attr('class', classes);
+            }
+        } else {
+          $(elem).attr('class', value);
+        }
+      }
+    }
+    return orig;
+  };
+
+  /* removeClass shim
+   ****************************************************/
+  var removeClass = $.fn.removeClass;
+  $.fn.removeClass = function(value) {
+    var orig = removeClass.apply(this, arguments);
+
+    var elem,
+      i = 0,
+      len = this.length;
+
+    for (; i < len; i++ ) {
+      elem = this[ i ];
+      if ( elem instanceof SVGElement ) {
+        var classes = $(elem).attr('class');
+        if ( classes ) {
+          var index = classes.indexOf(value);
+          if (index !== -1) {
+            classes = classes.substring(0, index) + classes.substring((index + value.length), classes.length);
+            $(elem).attr('class', classes);
+          }
+        }
+      }
+    }
+    return orig;
+  };
+
+  /* hasClass shim
+   ****************************************************/
+  var hasClass = $.fn.hasClass;
+  $.fn.hasClass = function(value) {
+    var orig = hasClass.apply(this, arguments);
+
+    var elem,
+      i = 0,
+      len = this.length;
+
+    for (; i < len; i++ ) {
+      elem = this[ i ];
+      if ( elem instanceof SVGElement ) {
+        var classes = $(elem).attr('class');
+
+        if ( classes ) {
+          if ( classes.indexOf(value) === -1 ) {
+            return false;
+          } else {
+            return true;
+          }
+        } else {
+            return false;
+        }
+      }
+    }
+    return orig;
+  };
+})(jQuery);
+
+// Interface handlers
 (function (window, document, $) {
 
   $(document).ready(function () {
@@ -12,7 +99,7 @@
   function initRadioHandler() {
     $('body').on('change', 'input[type="radio"]', function (ev) {
       $this = $(this);
-      $siblings = $this.closest('.js-trafficlight-widget').find('label');
+      $siblings = $this.closest('.js-radio-widget').find('label');
       $siblings.removeClass('selected');
       $this.closest('label').addClass('selected')
     });
@@ -76,7 +163,7 @@
 
     var spacelessName = name.replace(/\s/g,'')
 
-    var widget = '<div class="trafficlight-widget js-trafficlight-widget">\
+    var widget = '<div class="trafficlight-widget js-trafficlight-widget js-radio-widget">\
       <label class="trafficlight-widget_item trafficlight-widget_item--good" for="' + spacelessName + '_good_' + rand + '"><input id="' + spacelessName + '_good_' + rand + '" type="radio" name="' + name + '" value="1"> Good</label>\
       <label class="trafficlight-widget_item trafficlight-widget_item--some" for="' + spacelessName + '_some_' + rand + '"><input id="' + spacelessName + '_some_' + rand + '" type="radio" name="' + name + '" value="2"> Some</label>\
       <label class="trafficlight-widget_item trafficlight-widget_item--none" for="' + spacelessName + '_none_' + rand + '"><input id="' + spacelessName + '_none_' + rand + '" type="radio" name="' + name + '" value="3"> None</label>\
@@ -85,71 +172,175 @@
     $selector.append(widget);
   }
 
-  var $leftPupil = getPupulElement();
-  var $rightPupil = getPupulElement();
 
-  $('.js-eye-left').append($leftPupil);
-  $('.js-eye-right').append($rightPupil);
+}(window, document, jQuery));
 
-  $(document).mousemove(function(e){
-    moveEyePupil($leftPupil, e.pageX - window.pageXOffset, e.pageY - window.pageYOffset);
-    moveEyePupil($rightPupil, e.pageX - window.pageXOffset, e.pageY - window.pageYOffset);
-  });
+//Eyes
+(function (window, document, $) {
+  $(document).ready(function () {
+    var $leftPupil = getPupulElement();
+    var $rightPupil = getPupulElement();
 
-  function getPupulElement() {
-    return $('<div class="face_eye_pupil"></div>');
-  }
+    $('.js-eye-left').append($leftPupil);
+    $('.js-eye-right').append($rightPupil);
 
-  function moveEyePupil($element, cursorX, cursorY) {
-    // console.log(cursorX, cursorY);
-    var newTop, newLeft;
-    var pupilDiameter= $element.width(); //Assume the pupil is perfect circle
-    var parentOffset = $element.parent()[0].getBoundingClientRect();
-    var parentHeight = $element.parent().height();
+    $(document).mousemove(function(e){
+      moveEyePupil($leftPupil, e.pageX - window.pageXOffset, e.pageY - window.pageYOffset);
+      moveEyePupil($rightPupil, e.pageX - window.pageXOffset, e.pageY - window.pageYOffset);
+    });
 
-    // console.log("parentOffset", parentOffset.top);
-
-    function getOffset (cursor, side) {
-      if (cursor <= parentOffset[side]) {
-        return 0;
-      } else if (cursor > parentOffset[side] - pupilDiameter && cursor  <= parentOffset[side] + parentHeight - pupilDiameter) {
-        return cursor - parentOffset[side];
-      } else {
-        return parentHeight - pupilDiameter;
-      }
+    function getPupulElement() {
+      return $('<div class="face_eye_pupil"></div>');
     }
 
-    $element.css({
-      'top': getOffset(cursorY, 'top')+ 'px',
-      'left': getOffset(cursorX, 'left')+ 'px',
+    function moveEyePupil($element, cursorX, cursorY) {
+      // console.log(cursorX, cursorY);
+      var newTop, newLeft;
+      var pupilDiameter= $element.width(); //Assume the pupil is perfect circle
+      var parentOffset = $element.parent()[0].getBoundingClientRect();
+      var parentHeight = $element.parent().height();
+
+      // console.log("parentOffset", parentOffset.top);
+
+      function getOffset (cursor, side) {
+        if (cursor <= parentOffset[side]) {
+          return 0;
+        } else if (cursor > parentOffset[side] - pupilDiameter && cursor  <= parentOffset[side] + parentHeight - pupilDiameter) {
+          return cursor - parentOffset[side];
+        } else {
+          return parentHeight - pupilDiameter;
+        }
+      }
+
+      $element.css({
+        'top': getOffset(cursorY, 'top')+ 'px',
+        'left': getOffset(cursorX, 'left')+ 'px',
+      });
+    }
+  });
+}(window, document, jQuery));
+
+// Submit and step 2
+(function (window, document, $) {
+
+  var formData = [];
+
+  $(document).ready(function () {
+    $('.js-submit-step-1').on('click', function (ev) {
+      ev.preventDefault();
+      $this = $(this);
+      $form = $this.closest('form');
+      formData = $form.serializeArray();
+      console.log(JSON.stringify(formData));
+
+      $this.addClass('loading');
+
+      if (formData.length < 1) {
+        $this.removeClass('loading');
+        return;
+      }
+
+      var promise = $.ajax({
+        beforeSend: function(xhrObj){
+          xhrObj.setRequestHeader("Content-Type","application/json");
+          xhrObj.setRequestHeader("Accept","application/json");
+        },
+
+        // method: "POST",
+        // url: "/api/getSuggestions",
+
+        method: "GET",
+        url: "mock.json",
+
+        data: JSON.stringify(formData)
+      });
+
+      promise.done(function (response) {
+        $('#render-area').load('partials/page2.html', function () {
+          console.log("response", response);
+          window.scrollTo(0, 0);
+          buildMastheadList();
+          $('#featured-products-area').html(returnGadgetList(response.gadgetSet));
+          $('.js-video-list-small').html(returnVideoList(response.videoSet));
+          $('#featured-video-area').html(returnFeaturedVideo(response.videoSet[0]));
+        });
+      });
+
+      promise.fail(function (a,b) {
+        console.log("fail", a, b);
+        $this.removeClass('loading');
+      });
+    })
+  });
+
+  function buildMastheadList() {
+    var goodList = '';
+    var someList = '';
+    formData.forEach(function callback(currentValue, index, array) {
+      console.log(currentValue);
+      switch (parseInt(currentValue.value)) {
+        case 1:
+          goodList += '<li>' + currentValue.name + '</li>';
+          break;
+        case 2:
+          someList += '<li>' + currentValue.name + '</li>';
+          break;
+      }
+
+      if (goodList != '') {
+        $('.js-masthead-good-list').html(goodList);
+      } else {
+        $('.js-masthead-good-list-title').hide();
+        $('.js-masthead-good-list').hide();
+      }
+
+      if (someList != '') {
+        $('.js-masthead-some-list').html(someList);
+      } else {
+        $('.js-masthead-some-list-title').hide();
+        $('.js-masthead-some-list').hide();
+      }
     });
   }
 
-}(window, document, jQuery))
+  function returnFeaturedVideo(videoListItem) {
+    return '<div class="col-small-7 padded-none"><iframe class="video-large-iframe" src="https://www.youtube.com/embed/' + videoListItem.youtubeId + '?autoplay=0&amp;rel=0&amp;showinfo=0&amp;fs=1&amp;modestbranding=1" frameborder="0" allowfullscreen=""></iframe></div>\
+      <div class="col-small-5 text-color-white padded-double padded-top-quad">\<p>' + videoListItem.videoBlurb + '</p></div>';
+  }
 
-$(document).ready(function () {
-  $('.js-submit-step-1').on('click', function (ev) {
-    ev.preventDefault();
-    $this = $(this);
-    $form = $this.closest('form');
-    console.log(JSON.stringify($form.serializeArray()));
+  function returnVideoList(videoList) {
+    var returnString = '';
 
-    var promise = $.ajax({
-      beforeSend: function(xhrObj){
-        xhrObj.setRequestHeader("Content-Type","application/json");
-        xhrObj.setRequestHeader("Accept","application/json");
-      },
-      method: "POST",
-      url: "/api/getSuggestions",
-      data: JSON.stringify($form.serializeArray())
+    $.each(videoList, function(index, value) {
+      if (index > 3) {
+        return false;
+      }
+      if (index != 0) {
+        returnString += returnVideoListItem(value);
+      }
     });
 
-    promise.done(function (response) {
-      console.log(response);
-    });
+    return returnString;
+  }
 
-    promise.fail(function (a,b) {
-      console.log("fail", a, b);
-    });
-  })
-});
+  function returnVideoListItem(videoListItem) {
+    return '<div class="col-small-4">\
+      <div class="videos-large-thumb videos-large-thumb--small" style="background-image: url(\'//img.youtube.com/vi/' + videoListItem.youtubeId + '/0.jpg\');"></div>\
+        <p class="text-center">&nbsp;</p>\
+      </div>';
+  }
+
+  function returnGadgetList(gadgetList) {
+    return returnGadgetListItem(gadgetList[0]) + returnGadgetListItem(gadgetList[1]);
+  }
+
+  function returnGadgetListItem(gadgetListItem) {
+    return '<div class="col-small-6">\
+        <div class="gadget-card" style="background-image: url(\'' + gadgetListItem.pictureUrl + '\');">\
+          <div class="gadget-card_title">' + gadgetListItem.name + '</div>\
+        </div>\
+        <p>' + gadgetListItem.description + '</p>\
+      </div>';
+  }
+
+}(window, document, jQuery));
